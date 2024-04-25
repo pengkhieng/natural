2e9c9b2feb22b789ef6fb60baa97f8ba0be89210
pengkhieng <khieng.mantisy@gmail.com>
2024-04-25 11:49:42 +0700

    first inital

---
+++ src/main/java/com/natural/NaturalApplication.java
@@ -0,0 +1,13 @@
+package com.natural;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaturalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaturalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/natural/NaturalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.natural;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaturalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
